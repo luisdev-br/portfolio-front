cdca3e6fde3b48467c4b3dbf76030fa26a20771f
Luis Eduardo <132106812+luisdev-br@users.noreply.github.com>
2025-06-05 13:16:53 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,8 @@ export default defineConfig({
       localsConvention: "camelCase",
     },
   },
+  server: {
+    host: '0.0.0.0',  // Permite conexões externas
+    port: 5173,        // Certifique-se de que a porta é 5173 ou outra de sua escolha
+  },
 });
