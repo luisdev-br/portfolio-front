deda0ad8b5a1180bf0d50d169155c8077ea0ab35
Luis Eduardo <132106812+luisdev-br@users.noreply.github.com>
2025-06-10 18:42:49 -0300

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -2,4 +2,4 @@ export const getImageUrl = (path) => {
   return new URL(`/assets/${path}`, import.meta.url).href;
 };
 
-export const API_URL = 'http://3.16.137.128:80';
+export const API_URL = 'http://3.145.53.193:80';
